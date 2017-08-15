d260f072c79d4b9c6ef3aaaa4a3e7b70a479310a
Nekototori <jessykah.bird@gmail.com>
2017-08-15 15:58:11 -0400

    Added users class

--- manifests/site.pp
+++ manifests/site.pp
@@ -45,9 +45,5 @@ node default {
   include role::classroom
   notify { 'I made a change!': }
   
-  exec { 'cowsay motd':
-    command => "cowsay 'Welcome to ${facts['fqdn']}!' > /etc/motd",
-    path    => '/usr/local/bin',
-    creates => '/etc/motd',
-  }
+  include users
 }
