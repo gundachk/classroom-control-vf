81e2a7f0580110a82a91e5d49f38e7ff9b1841ea
Nekototori <jessykah.bird@gmail.com>
2017-08-15 15:07:35 -0400

    exec'd the motd

--- manifests/site.pp
+++ manifests/site.pp
@@ -45,10 +45,8 @@ node default {
   include role::classroom
   notify { 'I made a change!': }
   
-  file { '/etc/motd':
-    ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    content => 'I learned things',
+  exec { 'cowsay motd':
+    command => "cowsay 'Welcome to ${facts['fqdn']}!' > /etc/motd",
+    creates => '/etc/motd',
   }
 }
