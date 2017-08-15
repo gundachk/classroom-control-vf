267f2dda251a98b8804054aa5ec0381e8ee027dc
dinardoc <31038134+dinardoc@users.noreply.github.com>
2017-08-15 15:07:48 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -42,12 +42,9 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
-  file { '/etc/motd':  
-  ensure  => file,  
-  owner   => 'root',  
-  group   => 'root',  
-  mode    => '0644',  
-  content => "Werewolf is fun!\n",
-  }
+  exec { "cowsay motd":
+    command => "cowsay 'Welcome to ${facts['fqdn']}!' > /etc/motd",
+    creates => '/etc/motd'
+    }
   include role::classroom
 }
