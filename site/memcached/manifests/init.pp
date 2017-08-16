c52be9f57834fd5ec15a94e6d8c317f61d9cbc4b
shahso <31042336+shahso@users.noreply.github.com>
2017-08-16 11:38:50 -0400

    Update init.pp

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -16,6 +16,6 @@ class memcached {
   service { 'memcached':
       ensure => running,
       enable => true,
-      subscribe => File ['/etc/sysconfig/memcached'],
+      subscribe => File['/etc/sysconfig/memcached'],
   } 
 }
