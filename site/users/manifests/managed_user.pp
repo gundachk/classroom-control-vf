b6244b85725d0d040c2272b340f20fd00e3b6cd8
raghsingh <31039366+raghsingh@users.noreply.github.com>
2017-08-16 15:37:02 -0400

    Update managed_user.pp

--- site/users/manifests/managed_user.pp
+++ site/users/manifests/managed_user.pp
@@ -4,7 +4,7 @@ define users::managed_user (
   user { $title:
     ensure => present,
   }
-  file { ["/home/${title}], "/home/${title}/.ssh"]:
+  file { ["/home/${title}", "/home/${title}/.ssh"]:
     ensure => directory,
     owner => $title,
     group => $group,
