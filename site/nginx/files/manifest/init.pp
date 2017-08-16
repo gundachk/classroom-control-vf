34994ad65b266336af6449c9fb65e039edac66a5
shumk <kinlshum@gmail.com>
2017-08-16 12:00:57 -0400

    Update init.pp

--- site/nginx/files/manifest/init.pp
+++ site/nginx/files/manifest/init.pp
@@ -4,3 +4,4 @@ class nginx {    # Need to install the software  package { 'nginx':    ens
   # Require tells Puppet that the package comes first before the type.  # Notify tells Puppet that if this resource changes, it needs to reload the service in this case.  #  file { '/etc/nginx/nginx.conf':    source  => 'puppet:///modules/nginx/nginx.conf',    require => Package['nginx'],    notify  => Service['nginx'],  }
   file { '/etc/nginx/conf.d/default.conf':    source  => 'puppet:///modules/nginx/default.conf',    require => Package['nginx'],    notify  => Service['nginx'],  }
   service { 'nginx':    ensure => running,    enable => true,  }}
+
