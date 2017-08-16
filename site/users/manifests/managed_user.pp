f7b47917124193af51b960f7396101859fbfb80c
lbrinson <31038239+lbrinson@users.noreply.github.com>
2017-08-16 15:20:27 -0400

    Create managed_user.pp

---
+++ site/users/manifests/managed_user.pp
@@ -0,0 +1,13 @@
+define users::managed_user (
+  $group = $title
+ ) {
+  user { $title:
+    ensure => present,
+  }
+  
+  file { ["/home/${title}","/home/${title}/.ssh"]:
+    ensure => directory,
+    owner  => $title,
+    group  => $group,
+  }
+}
