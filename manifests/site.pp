41a896cccaa4e95d756b233f2256eb160a129afd
raghsingh <31039366+raghsingh@users.noreply.github.com>
2017-08-16 13:47:57 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -42,6 +42,10 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
+  if $::virtual != 'physical' {
+    $vmname = capitalize($::virtual)
+    notify { "This is a ${vmname} Virtual Machine.": }
+  }
   include role::classroom
   notify {"This is Raghav Singh's File modification TEST !!" : }
 
